33ded5837cd2783a5b8560a61be3b5cb5f19a0bc
Jasper Chen <takingblue.cs96@g2.nctu.edu.tw>
2013-12-15 12:35:19 +0800

    <Tool-to> modify as file base
    bookmark location: ~/.toolset/bookmarks

--- bookmark.sh
+++ bookmark.sh
@@ -1,33 +1,45 @@
 function to(){
-    if [ ! -e ~/.bookmarks ]; then
-        mkdir ~/.bookmarks
+    if [ ! -e ~/.toolset ]; then
+        mkdir ~/.toolset
     fi
     case $1 in
         -c)
-            if [ ! -f ~/.bookmarks/$2 ]; then
-                echo "cd `pwd`">~/.bookmarks/$2
+            if [ ! -f ~/.toolset/bookmarks ] ;then
+                echo "$2:cd `pwd`">~/.toolset/bookmarks
             else
-                echo "Bookmark $2 already exist"
+                Tmp=$2
+                Path=`awk -F: '{FS=":"}$1=="'"$Tmp"'"{print $2}' ~/.toolset/bookmarks`
+                if [ ! -z "$Path" ]; then
+                    echo "Bookmark $2 already exist"
+                else
+                    echo "$2:cd `pwd`" >> ~/.toolset/bookmarks
+                fi
             fi
             ;;
         -d)
-            if [ -f ~/.bookmarks/$2 ]; then
-                rm -f ~/.bookmarks/$2
+            if [ ! -f ~/.toolset/bookmarks ] ; then
+                echo "There is no bookmarks!"
             else
-                echo "$2 didn't exist"
+                sed '/^'"$2"'/d' ~/.toolset/bookmarks > ~/.toolset/bookmarks_tmp
+                mv ~/.toolset/bookmarks_tmp ~/.toolset/bookmarks
             fi
             ;;
         -l)  
-            ls -l ~/.bookmarks/
+            cat ~/.toolset/bookmarks
             ;;
         -h)
             echo "Usage: to [-c|-d|-l|-h] [bookmark], go to bookmark if without no option"
             ;;
         *)
-            if [ -f ~/.bookmarks/$1 ]; then
-                source ~/.bookmarks/$1
-            else
+            if [ -z $1 ] ; then
                 echo "Usage: to [-c|-d|-l|-h] [bookmark], go to bookmark if without no option"
+            elif [ ! -f ~/.toolset/bookmarks ] ;then
+                echo "There is no bookmarks!"
+                echo "Please use 'to -c' to create one. "
+            else
+                Tmp=$1
+                Path=`awk -F: '{FS=":"}$1=="'"$Tmp"'"{print $2}' ~/.toolset/bookmarks`
+                eval "$Path"
             fi
             ;;
     esac
